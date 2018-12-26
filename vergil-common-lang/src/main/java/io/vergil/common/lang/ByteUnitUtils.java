109fe4bc3ae1a0da0b189ca6606e11ca53260fe2
vergil <vergilchiu@foxmail.com>
2018-12-26 16:45:04 +0800

    fix size beyond TB bug

--- vergil-common-lang/src/main/java/io/vergil/common/lang/ByteUnitUtils.java
+++ vergil-common-lang/src/main/java/io/vergil/common/lang/ByteUnitUtils.java
@@ -4,18 +4,18 @@ import java.text.DecimalFormat;
 
 /**
  * 字节单位转换
- * 
+ *
  * @author zhaowei
  * @date 2015年12月8日下午7:03:57
  */
 public class ByteUnitUtils {
 
-	public static String format(long size) {
-		if (size <= 0) {
-			return "0B";
-		}
-		final String[] units = new String[] { "B", "kB", "MB", "GB", "TB" };
-		int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
-		return new DecimalFormat("#,##0.###").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
-	}
+    public static String format(long size) {
+        if (size <= 0) {
+            return "0B";
+        }
+        final String[] units = new String[]{"B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return new DecimalFormat("#,##0.###").format(size / Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+    }
 }
